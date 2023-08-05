9c0f4bc9ab2e3dd85563239fa531b7875841effc
sudo-Tiz <tiz@computiz>
2023-08-05 22:57:43 +0200

    do not swallow zbarcam

--- config.h
+++ config.h
@@ -68,14 +68,15 @@ static const Rule rules[] = {
      *	WM_CLASS(STRING) = instance, class
      *	WM_NAME(STRING) = title
      */
-    /* class	instance	title		tags mask	isfloating isterminal noswallow monitor */
-    {"Gimp", NULL, NULL, 1 << 8, 0, 0, 0, -1},
-    {NULL, NULL, "PlayOnLinux", 0, 1, 0, 0, -1},
-    {TERMCLASS, NULL, NULL, 0, 0, 1, 0, -1},
-    {NULL, NULL, "Event Tester", 0, 0, 0, 1, -1},
-    {TERMCLASS, "bg", NULL, 1 << 7, 0, 1, 0, -1},
-    {NULL, "spterm", NULL, SPTAG(0), 1, 1, 0, -1},
-    {NULL, "spcalc", NULL, SPTAG(1), 1, 1, 0, -1},
+    /* class    instance      title                   tags mask 	isfloating  isterminal  noswallow   monitor */
+    {"Gimp",    NULL,         NULL,                   1 << 8,     0,          0,          0,          -1},
+    {NULL,      NULL,         "PlayOnLinux",          0,          1,          0,          0,          -1},
+    {TERMCLASS, NULL,         NULL,                   0,          0,          1,          0,          -1},
+    {NULL,      NULL,         "Event Tester",         0,          0,          0,          1,          -1},
+    {TERMCLASS, "bg",         NULL,                   1 << 7,     0,          1,          0,          -1},
+    {NULL,      NULL,         "zbar barcode reader",  0,          0,          1,          1,          -1},
+    {NULL,      "spterm",     NULL,                   SPTAG(0),   1,          1,          0,          -1},
+    {NULL,      "spcalc",     NULL,                   SPTAG(1),   1,          1,          0,          -1},
 };
 
 /* layout(s) */
